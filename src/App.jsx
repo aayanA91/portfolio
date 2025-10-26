ff1a21a35889db79fcd501b017e20e8d16032806
aayanA91 <aayan.practical@gmail.com>
2025-10-27 01:32:01 +0500

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
 import { useState } from 'react'
 import Loader from './components/loader'
 import HackerHero from "./pages/home"
-
+import { Helmet } from "react-helmet";
 
 function App() {
  const [loaded, setLoaded] = useState(false);
 
   return (
     <>
+     <Helmet>
+        <title>Aayan Anees | Portfolio</title>
+        <meta name="description" content="Frontend & Full Stack Developer. Check my projects, skills, and achievements." />
+        
+        {/* Structured Data */}
+        <script type="application/ld+json">
+          {`
+          {
+            "@context": "https://schema.org",
+            "@type": "Person",
+            "name": "Aayan Anees",
+            "url": "https://aayan-portfolio-three.vercel.app/",
+            "sameAs": [
+              "https://github.com/aayanA91",
+              "https://www.linkedin.com/in/aayan-aness-b1a14231b/",
+              "https://twitter.com/aayanA91"
+            ],
+            "jobTitle": "Frontend Developer / Full Stack Developer",
+            "worksFor": {
+              "@type": "Organization",
+              "name": "Self-employed"
+            }
+          }
+          `}
+        </script>
+      </Helmet>
+
       {!loaded ? (
         <Loader onComplete={() => setLoaded(true)} />
       ) : (
