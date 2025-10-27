a28f45ecdede7aba22331b9b8a278a7095dd441f
aayanA91 <aayan.practical@gmail.com>
2025-10-28 00:04:08 +0500

    final

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -4,11 +4,7 @@ import { createWriteStream } from 'fs';
 const sitemap = new SitemapStream({ hostname: 'https://aayan-portfolio-three.vercel.app' });
 
 // Add your routes
-sitemap.write({ url: '/', changefreq: 'monthly', priority: 1.0 });
-sitemap.write({ url: '/about', changefreq: 'monthly', priority: 0.8 });
-sitemap.write({ url: '/projects', changefreq: 'monthly', priority: 0.8 });
-sitemap.write({ url: '/contact', changefreq: 'monthly', priority: 0.8 });
-sitemap.end();
+
 
 streamToPromise(sitemap).then((data) => {
   createWriteStream('./public/sitemap.xml').write(data.toString());
